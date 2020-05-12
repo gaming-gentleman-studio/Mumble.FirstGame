5f37afb9451b5ef07c43138f1e70715b4efb3118
Jason Heye <jason.heye@wsu.edu>
2020-05-12 01:37:35 -0700

    Partial Img implimentation

---
+++ Mumble.FirstGame.MonogameShared/ContentImages.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace Mumble.FirstGame.MonogameShared
+{
+    class ContentImages
+    {
+        public Texture2D ImgTheDude { get; set; }
+
+        ImgTheDude = Content.Load<Texture2D>("The Dude");
+    }
+}
